850227b6ca91df9bb7c12cb588aaa36169380970
Thanh Tu <tu@novalex.vn>
2020-05-16 00:05:11 +0700

    best model

--- finetune-date.sh
+++ finetune-date.sh
@@ -9,7 +9,7 @@ TRAINNED_TEXT=./eng.$MODEL_NAME.training_text
 # TRAINNED_TEXT=./eng.MRZ.training_text
 
 
-rm -rf ./$MODEL_NAME
+rm -rf ./${MODEL_NAME}
 ./tesstrain.sh \
 --fonts_dir ./.fonts \
 --lang eng --linedata_only \
@@ -21,32 +21,40 @@ rm -rf ./$MODEL_NAME
 --fontlist "Arial" \
 --training_text $TRAINNED_TEXT \
 --workspace_dir ./tmp \
---output_dir ./$MODEL_NAME
+--output_dir ./${MODEL_NAME}
 
 #  echo "/n ****** Finetune plus tessdata_best/eng model ***********"
 
-rm -rf  ./$MODEL_NAME_plus
-mkdir  ./$MODEL_NAME_plus
+rm -rf  ./${MODEL_NAME}_plus
+mkdir  ./${MODEL_NAME}_plus
 
 combine_tessdata -e $TRAINNED_DATA \
 $TESSDATA_DIR/eng.lstm
 
 lstmtraining \
---model_output ./$MODEL_NAME_plus/$MODEL_NAME_plus \
---traineddata ./$MODEL_NAME/eng/eng.traineddata \
+--model_output ./${MODEL_NAME}_plus/${MODEL_NAME}_plus \
+--traineddata ./${MODEL_NAME}/eng/eng.traineddata \
 --continue_from $TESSDATA_DIR/eng.lstm \
 --old_traineddata $TRAINNED_DATA \
---train_listfile ./$MODEL_NAME/eng.training_files.txt \
+--train_listfile ./${MODEL_NAME}/eng.training_files.txt \
 --debug_interval 0 \
 --max_iterations 2000
 
 
+# best model from checkpoint
+lstmtraining \
+--stop_training \
+--traineddata ./${MODEL_NAME}/eng/eng.traineddata \
+--continue_from ./${MODEL_NAME}_plus/${MODEL_NAME}_plus_checkpoint \
+--model_output ./${MODEL_NAME}_plus/${MODEL_NAME}.traineddata
 
+
+# fast model from checkpoint
 lstmtraining \
 --stop_training \
 --convert_to_int \
---traineddata ./$MODEL_NAME/eng/eng.traineddata \
---continue_from ./$MODEL_NAME_plus/$MODEL_NAME_plus_checkpoint \
---model_output ./$MODEL_NAME_plus/$MODEL_NAME_int.traineddata
+--traineddata ./${MODEL_NAME}/eng/eng.traineddata \
+--continue_from ./${MODEL_NAME}_plus/${MODEL_NAME}_plus_checkpoint \
+--model_output ./${MODEL_NAME}_plus/${MODEL_NAME}_int.traineddata
 
 
